f8b635eec7677f93baa2128101ab10048d5f3a86
NicolleInterior <intercoeli@gmail.com>
2021-08-21 13:24:51 +0800

    I replaced food.js with tasks.js schema

---
+++ server/models/Tasks.js
@@ -0,0 +1,15 @@
+// this is the schema that will be call in index.js
+// this is a json 
+
+const mongoose = require('mongoose');
+
+//create an object
+const taskSchema = new mongoose.Schema({
+    taskName: {
+        type: String,
+        required: true,
+    },
+});
+
+const Task = mongoose.model("tasks", taskSchema);
+module.exports = Task;
