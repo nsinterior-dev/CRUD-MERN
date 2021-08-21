e94535187acb618f67aff440c206c5c7db3d3dda
NicolleInterior <intercoeli@gmail.com>
2021-08-21 13:36:36 +0800

    I added the post and get inside server > index.js

--- server/index.js
+++ server/index.js
@@ -5,10 +5,40 @@ const cors = require('Cors');
 // app variable is needed to reference variable express that requires express.js
 const app = express();
 
+//calling the schema tasks.js\\
+const taskModel = require('./models/Tasks');
+
 // this line to call the schema for database
 app.use(express.json());
 app.use(cors());
 
+//initialize connection with the databasse
+mongoose.connect('mongodb+srv://admin:testpw1234@crudapp.agmbf.mongodb.net/taskdb?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+});
+
+
+app.post('/insert', async (req, res) => {
+    const taskName = req.body.taskName;
+    const task = new taskModel({taskName: 'task 1'});
+    try{
+        await task.save();
+        res.send("inserted data");
+    }
+    catch(err){
+        console.log(err);
+    }
+});
+
+app.get('/read', async (req, res) => {
+    taskModel.find({}, (err, result) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.send(result);
+    })
+})
 
 // initializes port 3001
 app.listen(3001, () => {
