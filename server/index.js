baf27de7de3db580e16a1b8b935e46f87b88a2f1
NicolleInterior <intercoeli@gmail.com>
2021-08-21 13:07:17 +0800

    initialize port 3001 in server > index.js

--- server/index.js
+++ server/index.js
@@ -5,5 +5,12 @@ const cors = require('Cors');
 // app variable is needed to reference variable express that requires express.js
 const app = express();
 
-
+// this line to call the schema for database
 app.use(express.json());
+app.use(cors());
+
+
+// initializes port 3001
+app.listen(3001, () => {
+    console.log(`Server started on port 3001`);
+});
