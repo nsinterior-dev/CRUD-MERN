a07b3c11c37b11ba8792fefceba63260b56c2b11
NicolleInterior <intercoeli@gmail.com>
2021-08-21 14:08:41 +0800

    I am now designing the front end with react-bootstrap. I started with the title. i will deal later with the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-
+// import stuff to call some stuff
+import React from 'react';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container'
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-     
+     <div className="container">
+        <h1>CRUD APP WITH MERN</h1>
+     </div>
+        
+
+  
     </div>
   );
 }
